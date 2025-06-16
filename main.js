b11465fcb95e0c23582351662156bf3bc7fdaeee
helenadot <eloiza.bueno@escola.pr.gov.br>
2025-06-16 09:02:06 -0300

    16/06 aula 4

--- main.js
+++ main.js
@@ -4,4 +4,18 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll (`.parametro-senha__botao`);
 
-console.log(botoes);
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTAmanho;
+
+function diminuiTamanho () {
+    if (tamanhoSenha > 1) {
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho () {
+    if (tamanhoSenha < 20){
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+numeroSenha.textContent = tamanhoSenha;
